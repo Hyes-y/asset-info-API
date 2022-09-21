1fc1422083ac790272f9a9762a0a328ba3aff969
Hyes-y <gptjs7970@naver.com>
2022-09-21 21:01:35 +0900

    feat: 투자 내역 조회, 상세 내역 조회 테스트 작성

--- apps/urls.py
+++ apps/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('users/', include('apps.user.urls')),
     path('accounts/', include('apps.account.urls')),
     path('assets/', include('apps.asset.urls')),
-    path('transaction/', include('apps.transaction.urls')),
+    path('transactions/', include('apps.transaction.urls')),
 ]
