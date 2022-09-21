0b90476d34f06f74a2d4bc36791859743c1979a1
Hyes-y <gptjs7970@naver.com>
2022-09-21 23:35:38 +0900

    chore: 사용하지 않는 파일 삭제

--- apps/utils.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-def check_pw(origin, hashed):
-    salt = bcrypt.gensalt()
-    origin_b = origin.encode('utf-8')
-    hashed = hashed.encode('utf-8')
-
-    origin_hashed = bcrypt.hashpw(origin_b, salt)
-
